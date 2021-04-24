cef2d8543f32f0db4eca665080dccec7b86e716c
ediarifigueiras <eaafs@iscte-iul.pt>
2021-04-24 23:30:42 +0100

    Extended classes of Composite.

--- src/pt/iscte/datamodel/JSONArray.kt
+++ src/pt/iscte/datamodel/JSONArray.kt
@@ -1,5 +1,22 @@
 package pt.iscte.datamodel
 
-class JSONArray {
-    val values: List<JSONValue> = mutableListOf<JSONValue>()
+import pt.iscte.visitor.Visitor
+
+class JSONArray() : Composite() {
+    override val elements: MutableList<JSONValue> = mutableListOf<JSONValue>()
+
+    override fun serialize(): String =
+        elements.joinToString (prefix = "[", separator = ", ", postfix = "]") { it.serialize() }
+
+    override fun accept(visitor: Visitor) {
+        if(visitor.visit(this))
+            elements.forEach {
+                it.accept(visitor)
+            }
+    }
+
+    fun add(element: JSONValue) {
+        elements.add(element)
+    }
+
 }
--- src/pt/iscte/datamodel/JSONObject.kt
+++ src/pt/iscte/datamodel/JSONObject.kt
@@ -1,5 +1,23 @@
 package pt.iscte.datamodel
 
-class JSONObject {
-    val unorderedSet: MutableMap<String, JSONValue> = mutableMapOf<String, JSONValue>()
+import pt.iscte.visitor.Visitor
+
+class JSONObject() : Composite() {
+    override val elements: MutableList<JSONObjectPair> = mutableListOf<JSONObjectPair>()
+
+    override fun accept(visitor: Visitor) {
+        if(visitor.visit(this))
+            elements.forEach {
+                it.accept(visitor)
+            }
+    }
+
+    override fun serialize(): String =
+        elements.joinToString (prefix = "{", separator = ", ", postfix = "}") { it.serialize() }
+
+
+    fun add(name: String, value: JSONValue) {
+        elements.add(JSONObjectPair(name, value, this))
+    }
+
 }
